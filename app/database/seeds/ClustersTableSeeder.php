13b3180a4dc6ebec3a877d89720096a6e200c319
Dai Dang Van <daikk115@gmail.com>
2015-10-09 22:10:11 +0700

    daidv: Done ClusterTableSeeder

--- app/database/seeds/ClustersTableSeeder.php
+++ app/database/seeds/ClustersTableSeeder.php
@@ -33,16 +33,132 @@ class ClustersTableSeeder extends Seeder {
                 'name'      => 'ĐH Sư phạm Hà Nội'
             ),
             array(
-                'code'      => 'admin',
-                'name'      => 'admin@example.org'
-            ),
-            array(
                 'code'      => 'LNH',
                 'name'      => 'ĐH Lâm nghiệp'
             ),
             array(
                 'code'      => 'NNH',
                 'name'      => 'HV Nông nghiệp Việt Nam'
+            ),
+            array(
+                'code'      => 'QGS',
+                'name'      => 'ĐH Quốc gia TP HCM'
+            ),
+            array(
+                'code'      => 'HUI',
+                'name'      => 'ĐH Công nghiệp TP HCM'
+            ),
+            array(
+                'code'      => 'SPK',
+                'name'      => 'ĐH S.Phạm kỹ thuật TP HCM'
+            ),
+            array(
+                'code'      => 'SPS',
+                'name'      => 'ĐH Sư phạm TP HCM'
+            ),
+            array(
+                'code'      => 'SGD',
+                'name'      => 'ĐH Sài Gòn'
+            ),
+            array(
+                'code'      => 'DTT',
+                'name'      => 'ĐH Tôn Đức Thắng'
+            ),
+            array(
+                'code'      => 'YDS',
+                'name'      => 'ĐH Y Dược TP HCM'
+            ),
+            array(
+                'code'      => 'DCT',
+                'name'      => 'ĐH CN thực phẩm TP HCM'
+            ),
+            array(
+                'code'      => 'HHA',
+                'name'      => 'ĐH Hàng hải Việt Nam'
+            ),
+            array(
+                'code'      => 'THP',
+                'name'      => 'ĐH Hải Phòng'
+            ),
+            array(
+                'code'      => 'TTB',
+                'name'      => 'ĐH Tây Bắc'
+            ),
+            array(
+                'code'      => 'DT',
+                'name'      => 'ĐH Thái Nguyên'
+            ),
+            array(
+                'code'      => 'TQU',
+                'name'      => 'ĐH Tân Trào'
+            ),
+            array(
+                'code'      => 'THV',
+                'name'      => 'ĐH Hùng Vương'
+            ),
+            array(
+                'code'      => 'YTB',
+                'name'      => 'ĐH Y Thái Bình '
+            ),
+            array(
+                'code'      => 'HDT',
+                'name'      => 'ĐH Hồng Đức '
+            ),
+            array(
+                'code'      => 'TDV',
+                'name'      => 'ĐH Vinh'
+            ),
+            array(
+                'code'      => 'DHU',
+                'name'      => 'ĐH Huế'
+            ),
+            array(
+                'code'      => 'DND',
+                'name'      => 'ĐH Đà Nẵng'
+            ),
+            array(
+                'code'      => 'DQN',
+                'name'      => 'ĐH Quy Nhơn'
+            ),
+            array(
+                'code'      => 'NLG',
+                'name'      => 'ĐH Nông lâm tại Gia Lai'
+            ),
+            array(
+                'code'      => 'TTN',
+                'name'      => 'ĐH Tây Nguyên'
+            ),
+            array(
+                'code'      => 'TDL',
+                'name'      => 'ĐH Đà Lạt'
+            ),
+            array(
+                'code'      => 'TSN',
+                'name'      => 'ĐH Nha Trang'
+            ),
+            array(
+                'code'      => 'TCT',
+                'name'      => 'ĐH Cần Thơ'
+            ),
+            array(
+                'code'      => 'SPD',
+                'name'      => 'ĐH Đồng Tháp'
+            ),
+            array(
+                'code'      => 'DVT',
+                'name'      => 'ĐH Trà Vinh '
+            ),
+            array(
+                'code'      => 'TTG',
+                'name'      => 'ĐH Tiền Giang'
+            ),
+            array(
+                'code'      => 'TAG',
+                'name'      => 'ĐH An Giang'
+            ),
+            array(
+                'code'      => 'DBL',
+                'name'      => 'ĐH Bạc Liêu'
             )
         );
 
