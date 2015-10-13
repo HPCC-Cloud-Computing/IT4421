a718851436c8c6103340d0b32d0173ab489fef72
TechBK <quangbinh.nguyentrong@gmail.com>
2015-10-13 13:28:52 +0700

    Binh: #14: create notices table

---
+++ app/database/migrations/2015_10_13_062659_create_notices_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNoticesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('notices', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->nullabletimestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('notices');
+	}
+
+}
