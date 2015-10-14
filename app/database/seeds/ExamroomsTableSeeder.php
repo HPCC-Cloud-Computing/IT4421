01337af6c0e5add8de2e308cacddbfe920cbd55d
Dai Dang Van <daikk115@gmail.com>
2015-10-14 10:29:13 +0700

    daidv: them clusterID

--- app/database/seeds/ExamroomsTableSeeder.php
+++ app/database/seeds/ExamroomsTableSeeder.php
@@ -9,18 +9,22 @@ Class ExamroomsTableSeeder extends Seeder{
 			array(
                 'code'      => 'HUST001',
                 'address'      => 'D9 501 ĐH Bách Khoa Hà Nội, cho leo tầng 5 nhé =))'
+                'cluster_id' => 1
             ),
             array(
                 'code'      => 'HUST002',
                 'address'      => 'D9 502 ĐH Bách Khoa Hà Nội, cho leo tầng 5 nhé =))'
+                'cluster_id' => 1
             ),
             array(
                 'code'      => 'HUST003',
                 'address'      => 'Phòng 201 THPT Phương Mai'
+                'cluster_id' => 1
             ),
             array(
                 'code'      => 'HUST004',
                 'address'      => 'Phòng 202 THPT Phương Mai'
+                'cluster_id' => 1
             )
 		);
 		DB::table('rooms')->insert( $array );
