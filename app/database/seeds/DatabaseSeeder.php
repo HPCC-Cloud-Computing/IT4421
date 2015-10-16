a33a1f4bbfd0034211bc44314895609d3cb9f3ff
Dai Dang Van <daikk115@gmail.com>
2015-10-16 23:04:42 +0700

    daidv: gọi các seeder đã viết

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder {
 		$this->call('MajorsTableSeeder');
 		$this->call('DepartmentsTableSeeder');
 		$this->call('StudentsTableSeeder');
+		$this->call('UsersTableSeeder');
 	}
 
 }
