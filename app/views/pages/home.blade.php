ad0d0336910005c52769593a5cfac3304f3db513
ngocluanbka <ngocluan.bka@gmail.com>
2015-10-27 02:29:34 +0700

    chinh sua lai thanh navigator

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -4,41 +4,51 @@
 @stop
 
 @section('navigator')
-	
-<nav class="navbar navbar-default navbar-static-top custom-navbar" role="navigation">
+
+<nav class="navbar navbar-default">
   <div class="container">
-    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-items">
-      <span class="sr-only">Toggle navigation</span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-    </button>
+    <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
-      <a class="navbar-brand" rel="home" href="#" title="Help">
-        BGDDT
-      </a>
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">BGDDT</a>
     </div>
 
-    <!-- Non-collapsing right-side icons -->
-    <ul class="nav navbar-nav navbar-right">
-
-      <li>
-        <a href="#" class="fa fa-home"></a>
-      </li>
-    </ul>
-    
-    <!-- the collapsing menu -->
-    <div class="collapse navbar-collapse navbar-left" id="nav-items">
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Trang chủ</a></li>
-        <li><a href="#">Tra cứu điểm thi</a></li>
-        <li><a href="#">Liên hệ</a></li>
+        <li class="active"><a href="#">Trang Chu</a></li>
+        <li><a href="#">Lien He</a></li>
       </ul>
-    </div>
-    <!--/.nav-collapse -->
-  </div>
-  <!--/.container -->
+      <form class="navbar-form navbar-left hidden-xs hidden-sm" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Tim kiem....">
+        </div>
+        <button type="submit" class="btn btn-default">
+            <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+        </button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li class=" hidden-xs"><a href="#">Xin chào, Luân</a></li>
+
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Tai Khoan <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Thong tin tai khoan</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Dang xuat</a></li>
+          </ul>
+        </li>
+
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
 </nav>
+
 
 
 @stop
