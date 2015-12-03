62e674aa74ae1f0eece47246e2d1072f77b42c83
Dai Dang Van <daikk115@gmail.com>
2015-12-03 09:50:20 +0700

    daidv: update md5 password

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -8,42 +8,42 @@ Class UsersTableSeeder extends Seeder{
 		$users = array(
 			array(
                 'username'      => 'bka2016',
-                'password'      => 'bka@2016',
+                'password'      => md5('bka@2016'),
                 'email' => 'bka@mail.gov.vn',
                 'userable_id'   => 1,
                 'userable_type' => 'cluster'
             ),
             array(
                 'username'      => 'kha2016',
-                'password'      => 'kha@2016',
+                'password'      => md5('kha@2016'),
                 'email' => 'kha@mail.gov.vn',
                 'userable_id'   => 2,
                 'userable_type' => 'cluster'
             ),
             array(
                 'username'      => 'hndepartment',
-                'password'      => '123456',
+                'password'      => md5('123456'),
                 'email' => 'hn_department@mail.gov.vn',
                 'userable_id'   => 62,
                 'userable_type' => 'department'
             ),
             array(
                 'username'      => 'daidv',
-                'password'      => 'vandai123',
+                'password'      => md5('vandai123'),
                 'email' => 'daikk11@gmail.com',
                 'userable_id'   => 1,
                 'userable_type' => 'student'
             ),
             array(
                 'username'      => 'kiennv',
-                'password'      => '123456',
+                'password'      => md5('123456'),
                 'email' => 'kienv@gmail.com',
                 'userable_id'   => 2,
                 'userable_type' => 'student'
             ),
             array(
                 'username'      => 'binhnq',
-                'password'      => '123456',
+                'password'      => md5('123456'),
                 'email' => 'binh@gmail.com',
                 'userable_id'   => 13,
                 'userable_type' => 'student'
