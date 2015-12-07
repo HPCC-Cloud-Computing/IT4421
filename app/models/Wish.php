84f1e402df7a1e4817ad260654bb2b0a9d43c194
Dai Dang Van <daikk115@gmail.com>
2015-12-07 22:23:59 +0700

    daidv: add number_order to Wish.php-fillable

--- app/models/Wish.php
+++ app/models/Wish.php
@@ -2,7 +2,7 @@
 
 class Wish extends Eloquent {
 
-	protected $fillable = array('student_id', 'major_id', 'sumscore');
+	protected $fillable = array('student_id', 'major_id', 'number_order', 'sumscore');
 
 	protected $table = 'wishs';
 }
