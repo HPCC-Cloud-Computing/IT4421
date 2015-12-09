7bc45a6419f624399b48e173590487842eac8097
Dai Dang Van <daikk115@gmail.com>
2015-12-09 13:01:36 +0700

    daidv: fix create user table: unique

--- app/database/migrations/2015_09_30_164710_create_users_table.php
+++ app/database/migrations/2015_09_30_164710_create_users_table.php
@@ -16,9 +16,9 @@ class CreateUsersTable extends Migration {
 		Schema::create('users', function (Blueprint $table) {
 
             $table->increments('id');
-            $table->string('username',50);
+            $table->string('username',50)->unique();;
             $table->string('password',64);
-            $table->string('email', 64);
+            $table->string('email', 64)->unique();;
             $table->morphs('userable');
             //$table->integer('role_id');
             //$table->integer('profile_id');
