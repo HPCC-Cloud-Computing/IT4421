5e40bcb75271118a64b7adbb56b44ea2a2088cd6
nhatbkk57 <nhat.hqh@gmail.com>
2015-12-13 21:48:24 +0700

    update manage phase

--- app/models/Wish.php
+++ app/models/Wish.php
@@ -2,15 +2,9 @@
 
 class Wish extends Eloquent {
 
-<<<<<<< HEAD
 	protected $fillable = array('student_id', 'major_id','combination_name','number_order', 'sumscore');
 
 	protected $table = 'wishs';
-=======
-	protected $fillable = array('student_id', 'major_id', 'combination_name', 'number_order', 'sumscore');
-
-	protected $table = 'wishs';
-
->>>>>>> ed09144ae03d61a0b222580d93b112beefe54e5f
+	
 	public $timestamps = false;
 }
